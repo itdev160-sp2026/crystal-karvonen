a95cca7aed44a7eea92355b93464ab363dd04323
Crystal Karvonen <karvonc1@matc.edu>
2026-02-03 19:38:06 -0600

    Completed Activity 1

---
+++ activity-1/scripts.js
@@ -0,0 +1,43 @@
+//Activity One
+
+console.log("Hello World!");
+
+document.getElementById("output").innerHTML = "<h2>Hello World!</h2>";
+
+let studentName = "Crystal";
+const age = 24;
+let isStudent = true;
+let emptyValue = null;
+let notAssigned;
+
+console.log("----Variable Values----");
+console.log("Student Name: ", studentName);
+console.log("Student Age: ", age);
+console.log("Is Student: ", isStudent);
+console.log("Empty Value: ", emptyValue);
+console.log("Not Assigned: ", notAssigned);
+
+console.log("----Variable Types----");
+console.log("typeof studentName: ", typeof studentName);
+console.log("typeof age: ", typeof age);
+console.log("typeof isStudent: ", typeof isStudent);
+console.log("typeof emptyValue: ", typeof emptyValue);
+console.log("typeof notAssigned: ", typeof notAssigned);
+
+console.log("---Variable Reassignment---");
+console.log("Original studentName: ", studentName);
+studentName = "Sally";
+console.log("New studentName: ", studentName);
+
+
+
+
+
+
+
+
+
+
+
+
+
